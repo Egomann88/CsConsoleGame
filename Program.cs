39a5af27e208689674664f3068c78247db7f3e0a
Justin <justin@kauz.ch>
2022-09-14 10:24:33 +0200

    Programm start created

--- Program.cs
+++ Program.cs
@@ -10,8 +10,33 @@ namespace RpgGame
     {
         static void Main(string[] args)
         {
+            char input = '0';
+            bool chAlive = false;
+            //Character character = new Character();
 
-            Console.ReadKey();  // endl
+            do {
+                // create character
+
+                do {
+                    Console.Clear();
+                    Console.WriteLine("Hauptmenü\n");
+                    Console.WriteLine("1) Dungeon\n2) Charakter betrachten\n8) neuen Charakter erstellen\n9) Spiel beenden");
+                    input = Console.ReadKey(true).KeyChar;
+
+                    switch(input) {
+                        case '1': break;
+                        case '2': break;
+                        case '8': chAlive = false; break;
+                        case '9': Environment.Exit(0); break;   // stops appligation
+                        default: break; // nothing happens
+                    }
+                } while (chAlive);  // if char still alive, start neu opt
+
+            } while (true);
+
+            Console.WriteLine("\n\n" + input);
+
+            Console.ReadKey(true);  // endl
         }
     }
 }
