2cd310649127677341d01a8b734f4932c9bfffed
Justin <justin@kauz.ch>
2022-09-14 15:39:26 +0200

    First letter of Name is always big

--- Program.cs
+++ Program.cs
@@ -21,10 +21,19 @@ namespace RpgGame
                 Console.Clear();
                 Console.WriteLine("Geben Sie den Namen ihres Charakters ein:");
                 name = Console.ReadLine();
+
                 if (name == "") {
                     Console.WriteLine("\nDer Name darf nicht leer sein!");
                     Thread.Sleep(500);
+                    continue;
                 }
+
+                // convert to char array of the string
+                char[] letters = name.ToCharArray();
+                // upper case the first char
+                letters[0] = char.ToUpper(letters[0]);
+                // put array back together
+                name = new string(letters);
             } while (name == "");
 
             do {
@@ -34,29 +43,6 @@ namespace RpgGame
             } while (cl < 1 && cl > 4);
 
             return new Character(name, cl);
-        }
-
-        private static void ShowCharacter(Character c) {
-            string cl = "";
-            if (c.Class == 1) cl = "Krieger";
-            else if (c.Class == 2) cl = "Magier";
-            else cl = "Schurke";
-
-            do {
-                Console.Clear();
-                Console.WriteLine("Name:\t\t\t{0}", c.Name);
-                Console.WriteLine("Klasse:\t\t\t{0}", cl);
-                Console.WriteLine("Level:\t\t\t{0}", c.Lvl);
-                Console.WriteLine("Exp:\t\t\t{0} / {1}", c.Exp[0], c.Exp[1]);
-                Console.WriteLine("Leben:\t\t\t{0} / {1}", c.Health[0], c.Health[1]);
-                Console.WriteLine("Gold:\t\t\t{0}", c.Gold);
-                Console.WriteLine("Stärke:\t\t\t{0}", c.Strength);
-                Console.WriteLine("Inteligents:\t\t{0}", c.Intelligents);
-                Console.WriteLine("Geschwindigkeit:\t{0}", c.Dexterity);
-                Console.WriteLine("Krit. Chance:\t\t{0} %", c.CritChance);
-                Console.WriteLine("Krit. Schaden:\t\t{0} %", (c.CritDmg - 1.0F) * 100);
-                Console.WriteLine("\nDrücken Sie <Enter> um zurückzukehren...");
-            } while (Console.ReadKey(false).Key != ConsoleKey.Enter);
         }
 
         private static void Dungeon(Character c) {
@@ -114,7 +100,7 @@ namespace RpgGame
 
                     switch(input) {
                         case '1': Dungeon(character); break;
-                        case '2': ShowCharacter(character); break;
+                        case '2': character.ShowCharacter(); break;
                         case '8': chAlive = false; break;
                         case '9': Environment.Exit(0); break;   // stops appligation
                         default: break; // nothing happens
