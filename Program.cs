4f03b58ae49bf37109d60fc9af7fdbefeb157119
Justin <justin@kauz.ch>
2022-09-14 11:42:50 +0200

    Character stats visible

--- Program.cs
+++ Program.cs
@@ -31,15 +31,31 @@ namespace RpgGame
                 Console.Clear();
                 Console.WriteLine("Was ist die Klasse ihres Charakters?\n1) Krieger\n2) Magier\n3) Schurke");
                 cl = Convert.ToByte(Console.ReadKey(false).KeyChar - 48);
-            } while (cl < 0 && cl > 4);
+            } while (cl < 1 && cl > 4);
 
             return new Character(name, cl);
         }
 
         private static void ShowCharacter(Character c) {
-            do {
-                Console.WriteLine("");
+            string cl = "";
+            if (c.Class == 1) cl = "Krieger";
+            else if (c.Class == 2) cl = "Magier";
+            else cl = "Schurke";
 
+            do {
+                Console.Clear();
+                Console.WriteLine("Name:\t\t\t{0}", c.Name);
+                Console.WriteLine("Klasse:\t\t\t{0}", cl);
+                Console.WriteLine("Level:\t\t\t{0}", c.Lvl);
+                Console.WriteLine("Exp:\t\t\t{0} / {1}", c.Exp[0], c.Exp[1]);
+                Console.WriteLine("Leben:\t\t\t{0} / {1}", c.Health[0], c.Health[1]);
+                Console.WriteLine("Gold:\t\t\t{0}", c.Gold);
+                Console.WriteLine("Stärke:\t\t\t{0}", c.Strength);
+                Console.WriteLine("Inteligents:\t\t{0}", c.Intelligents);
+                Console.WriteLine("Geschwindigkeit:\t{0}", c.Dexterity);
+                Console.WriteLine("Krit. Chance:\t\t{0} %", c.CritChance);
+                Console.WriteLine("Krit. Schaden:\t\t{0} %", (c.CritDmg - 1.0F) * 100);
+                Console.WriteLine("\nDrücken Sie <Enter> um zurückzukehren...");
             } while (Console.ReadKey(false).Key != ConsoleKey.Enter);
         }
 
@@ -56,7 +72,7 @@ namespace RpgGame
 
                 do {
                     Console.Clear();
-                    Console.WriteLine("Hauptmenü\n{0} bei Ihnen liegt die Wahl.", character.Name);
+                    Console.WriteLine("Hauptmenü\n{0}, bei Ihnen liegt die Wahl.", character.Name);
                     Console.WriteLine("1) Dungeon\n2) Charakter betrachten\n8) neuen Charakter erstellen\n9) Spiel beenden");
                     input = Console.ReadKey(true).KeyChar;
 
