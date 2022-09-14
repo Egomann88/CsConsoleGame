5a26da3c782edd66d2bc56aba37eda14cf026dbf
Justin <justin@kauz.ch>
2022-09-14 11:02:23 +0200

    Character creator created

--- Program.cs
+++ Program.cs
@@ -3,29 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace RpgGame
 {
     internal class Program
     {
+        /// <summary>
+        /// Creates an new Character with Name and Class.<br />
+        /// </summary>
+        /// <returns>Character</returns>
+        private static Character CharCreator() {
+            string name = "";
+            byte cl = 0;
+
+            do {
+                Console.Clear();
+                Console.WriteLine("Geben Sie den Namen ihres Charakters ein:");
+                name = Console.ReadLine();
+                if (name == "") {
+                    Console.WriteLine("\nDer Name darf nicht leer sein!");
+                    Thread.Sleep(500);
+                }
+            } while (name == "");
+
+            do {
+                Console.Clear();
+                Console.WriteLine("Was ist die Klasse ihres Charakters?\n1) Krieger\n2) Magier\n3) Schurke");
+                cl = Convert.ToByte(Console.ReadKey(false).KeyChar - 48);
+            } while (cl < 0 && cl > 4);
+
+            return new Character(name, cl);
+        }
+
+        private static void ShowCharacter(Character c) {
+            do {
+                Console.WriteLine("");
+
+            } while (Console.ReadKey(false).Key != ConsoleKey.Enter);
+        }
+
         static void Main(string[] args)
         {
             char input = '0';
             bool chAlive = false;
-            //Character character = new Character();
+            Character character;
 
             do {
                 // create character
+                character = CharCreator();
+                chAlive = true;
 
                 do {
                     Console.Clear();
-                    Console.WriteLine("Hauptmenü\n");
+                    Console.WriteLine("Hauptmenü\n{0} bei Ihnen liegt die Wahl.", character.Name);
                     Console.WriteLine("1) Dungeon\n2) Charakter betrachten\n8) neuen Charakter erstellen\n9) Spiel beenden");
                     input = Console.ReadKey(true).KeyChar;
 
                     switch(input) {
                         case '1': break;
-                        case '2': break;
+                        case '2': ShowCharacter(character); break;
                         case '8': chAlive = false; break;
                         case '9': Environment.Exit(0); break;   // stops appligation
                         default: break; // nothing happens
@@ -34,9 +71,6 @@ namespace RpgGame
 
             } while (true);
 
-            Console.WriteLine("\n\n" + input);
-
-            Console.ReadKey(true);  // endl
         }
     }
 }
