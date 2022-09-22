1de4e34481e71ad959808af7c4728fac89c4c996
Justin <justin@kauz.ch>
2022-09-22 07:39:49 +0200

    start/end fight & return is character died

--- Program.cs
+++ Program.cs
@@ -45,31 +45,47 @@ namespace RpgGame
             return new Character(name, cl);
         }
 
-        private static void Dungeon(Character c) {
+        /// <summary>
+        /// Check and returns if Character is alive
+        /// </summary>
+        /// <param name="c">character</param>
+        /// <returns>true, if yes - false, if not</returns>
+        private static bool IsCharacterAlive(Character c) {
+            if (c.Health[0] <= 0)
+                return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Generates rnd Enemy and let it be fought by the player
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns>character with new stats</returns>
+        private static Character Dungeon(Character c) {
             Random r = new Random();
-            Fight f = new Fight();
             byte rnd = 0;
 
-            if (c.Lvl < 10) rnd = Convert.ToByte(r.Next(1, 5));
+            if (c.Lvl < 10) rnd = Convert.ToByte(r.Next(1, 5)); // only picks the easy enemys
             else {
                 double rndDouble = r.NextDouble();
 
-                if (rndDouble >= 0.26) rnd = 1;
-                else if (rndDouble >= 0.2) rnd = 2;
-                else if (rndDouble >= 0.16) rnd = 3;
-                else if (rndDouble >= 0.12) rnd = 4;
-                else if (rndDouble >= 0.10) rnd = 5;
-                else if (rndDouble >= 0.075) rnd = 6;
-                else if (rndDouble >= 0.06) rnd = 7;
-                else if (rndDouble >= 0.015) rnd = 8;
-                else if (rndDouble >= 0.006) rnd = 9;
-                else if (rndDouble >= 0.004) rnd = 10;
+                if (rndDouble >= 0.26) rnd = 1; // 26 %
+                else if (rndDouble >= 0.2) rnd = 2; // 20 %
+                else if (rndDouble >= 0.16) rnd = 3; // 16 %
+                else if (rndDouble >= 0.12) rnd = 4; // 12 %
+                else if (rndDouble >= 0.10) rnd = 5; // 10 %
+                else if (rndDouble >= 0.075) rnd = 6; // 7.5 %
+                else if (rndDouble >= 0.06) rnd = 7; // 6 %
+                else if (rndDouble >= 0.015) rnd = 8; // 1.5 %
+                else if (rndDouble >= 0.006) rnd = 9; // 0.6 %
+                else if (rndDouble >= 0.004) rnd = 10; // 0.4 %
             }
 
-            Enemy e = new Enemy(c.Lvl, rnd, false);
+            Enemy e = new Enemy(c.Lvl, rnd, false); // generate enemy
+            Fight f = new Fight(c, e);  // generate fight
 
-            f.Fightin(c, rnd, false);
-            
+            return f.FightIn();
         }
 
         static void Main(string[] args) {
@@ -89,13 +105,16 @@ namespace RpgGame
                     input = Console.ReadKey(true).KeyChar;
 
                     switch(input) {
-                        case '1': Dungeon(character); break;
+                        case '1':
+                            character = Dungeon(character);
+                            chAlive = IsCharacterAlive(character);
+                            break;
                         case '2': character.ShowCharacter(); break;
                         case '8': chAlive = false; break;
                         case '9': Environment.Exit(0); break;   // stops appligation
                         default: break; // nothing happens
                     }
-                } while (chAlive);  // if char still alive, start neu opt
+                } while (chAlive);  // if char still alive, start new opt
 
             } while (true);
 
